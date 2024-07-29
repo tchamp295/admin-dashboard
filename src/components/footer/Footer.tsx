fb9614639946ee3d13f82d8850df9c9e243befab
victor <nyakundivictorn@gmail.com>
2024-07-30 00:16:27 +0300

    Completed ui design

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,8 @@ import "./footer.scss"
 const Footer = () => {
   return (
     <div className="footer">
-      <span>lamadmin</span>
-      <span>© Lama Dev Admin Dashboard</span>
+      <span>Admin</span>
+      <span>© Admin Dashboard</span>
     </div>
   )
 }
